989787327d0bd14b857e3060683087b375f3e914
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-09-17 16:00:03 -0700

    Pomodoro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,106 @@
-import Image from 'next/image'
+'use client';
+import React, { useEffect } from 'react';
+import { Button } from '@/components/ui/button';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Progress } from '@/components/ui/progress';
+import { useToast } from '@/components/ui/use-toast';
+import InfoAccordion from '@/components/InfoAccordion';
 
 export default function Home() {
+  const [seconds, setSeconds] = React.useState(1500);
+  const [timerType, setTimerType] = React.useState('25');
+  const [isTimerActive, setIsTimerActive] = React.useState(false);
+  const { toast } = useToast();
+
+  const leadingZeros = new Intl.NumberFormat('en-US', {
+    minimumIntegerDigits: 2,
+  });
+
+  function handleTabChange(e: string) {
+    setIsTimerActive(false);
+    setSeconds(parseInt(e) * 60);
+    setTimerType(e);
+  }
+
+  useEffect(() => {
+    if (!isTimerActive) return;
+    if (seconds <= 0) {
+      setIsTimerActive(false);
+      setSeconds(parseInt(timerType));
+      toast({
+        description: 'Timer complete!',
+      });
+    }
+
+    const timeoutId = setTimeout(timer, 1000);
+
+    function timer() {
+      setSeconds((s) => s - 1);
+      if (seconds <= 0) {
+        setIsTimerActive(false);
+      }
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [seconds, isTimerActive, timerType, toast]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <Progress
+        value={(1 - seconds / parseInt(timerType)) * 100}
+        className='sticky top-0'
+      />
+      <main className='center flex min-h-[75vh] flex-col'>
+        <Tabs
+          value={timerType}
+          defaultValue='25'
+          className={`center flex flex-col rounded-lg border p-4 shadow-md transition-all ${
+            isTimerActive
+              ? 'border-blue-100 dark:border-blue-400'
+              : 'border-slate-300 dark:border-slate-800'
+          }`}
+          onValueChange={(e: string) => handleTabChange(e)}
+        >
+          <TabsList className='shadow-sm'>
+            <TabsTrigger value='25'>Timer</TabsTrigger>
+            <TabsTrigger value='5'>Short Rest</TabsTrigger>
+            <TabsTrigger value='15'>Long Rest</TabsTrigger>
+          </TabsList>
+          <TabsContent className='tabs_content' value='25'>
+            <span>
+              {leadingZeros.format(Math.floor(seconds / 60))}:
+              {leadingZeros.format(seconds % 60)}
+            </span>
+          </TabsContent>
+          <TabsContent className='tabs_content' value='5'>
+            <span>
+              {leadingZeros.format(Math.floor(seconds / 60))}:
+              {leadingZeros.format(seconds % 60)}
+            </span>
+          </TabsContent>
+          <TabsContent className='tabs_content' value='15'>
+            <span>
+              {leadingZeros.format(Math.floor(seconds / 60))}:
+              {leadingZeros.format(seconds % 60)}
+            </span>
+          </TabsContent>
+          <Button
+            variant='secondary'
+            className='shadow-sm'
+            onClick={() => {
+              setIsTimerActive(!isTimerActive);
+              toast({
+                description: isTimerActive ? 'Timer stopped' : 'Timer started',
+              });
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            {isTimerActive ? 'Pause' : 'Start'}
+          </Button>
+        </Tabs>
+        <InfoAccordion />
+      </main>
+    </>
+  );
 }
