0ca5d625cbbf37080d76bd3fc1ccdc43a0e7c262
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-09-17 03:41:31 -0700

    Shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import { ThemeProvider } from '@/components/ThemeProvider';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -15,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
