1b8f631002488b0611d280bb41b15c2434b24e5f
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-09-17 16:00:10 -0700

    Info Accordion

---
+++ src/components/InfoAccordion/index.tsx
@@ -0,0 +1,55 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+
+export default function InfoAccordion() {
+  return (
+    <Accordion type='single' collapsible className='fixed bottom-0 min-w-[65ch]'>
+      <AccordionItem value='item-1'>
+        <AccordionTrigger>What is the Pomodoro Technique?</AccordionTrigger>
+        <AccordionContent>
+          <p>
+          The Pomodoro Technique is a time management method developed by
+          Francesco Cirillo in the late 1980s. It uses a timer to break work
+          into intervals, typically 25 minutes in length, separated by short
+          breaks.
+          </p>
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value='item-2'>
+        <AccordionTrigger>How does it work?</AccordionTrigger>
+        <AccordionContent>
+          <ol>
+            <li>
+              Decide on the task to be done. Set the Pomodoro timer (typically
+              for 25 minutes).
+            </li>
+            <li>Work on the task.</li>
+            <li>
+              End work when the timer rings and take a short break (typically
+              5–10 minutes)
+            </li>
+            <li>
+              Go back to Step 2 and repeat until you complete four pomodoros.
+            </li>
+            <li>
+              After four pomodoros are done, take a long break (typically 20 to
+              30 minutes) instead of a short break.
+            </li>
+            <li>Once the long break is finished, return to step 2.</li>
+          </ol>
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value='item-3'>
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
